2ef1c0fa77b7a27abb5b01ff55d5051fdfc14da8
ash-shaik <fastmlai777@gmail.com>
2021-12-14 22:22:57 -0800

    window functions

--- window-functions_01.sql
+++ window-functions_01.sql
@@ -23,3 +23,20 @@ SELECT DISTINCT post_type
 FROM tali_walt.wp_posts
     WINDOW W1 as (PARTITION by post_type), W2 as ()
 ORDER BY post_type;
+
+-- Assign a progressive number for each row within the partition.
+SELECT post_type
+     , ROW_NUMBER(*) over W
+from tali_walt.wp_posts
+    WINDOW W as (PARTITION by post_type)
+order by post_type;
+
+-- To get the first and last value with in the partition.
+SELECT post_type
+     , ROW_NUMBER() over W
+     , post_title
+     , FIRST_VALUE(post_title) OVER W
+     , LAST_VALUE(post_title) OVER W
+FROM tali_walt.wp_posts
+    WINDOW W AS (PARTITION BY post_type ORDER BY post_type)
+ORDER BY post_type;
