24f861540c63ebf408e5a652dcf8942400cb5736
ash-shaik <fastmlai777@gmail.com>
2021-12-15 14:22:21 -0800

    random sampling

--- random-sampling.sql
+++ random-sampling.sql
@@ -8,13 +8,21 @@ SELECT id,
 FROM tutorial.billboard_top_100_year_end TABLESAMPLE BERNOULLI(1)
 LIMIT 1;
 
+-- When we are looking for an efficient query, this will still do a table scan.
 SELECT id,
        song_name
 FROM tutorial.billboard_top_100_year_end
 WHERE id >= (
-    SELECT random() * (max(id) - min(id)) + min(id)
+    SELECT FLOOR(random() * (max(id) - min(id) + 1) + min(id))
     FROM tutorial.billboard_top_100_year_end
 )
 ORDER BY id
 LIMIT 1;
 
+-- A better approach could be to use a subquery and JOIN to avoid the table scan.
+SELECT id, song_name
+FROM tutorial.billboard_top_100_year_end tb1
+         JOIN
+     (SELECT FLOOR(min(id) + (max(id) - min(id) + 1) * RANDOM()) AS id2
+      FROM tutorial.billboard_top_100_year_end) tb2 ON tb1.id = tb2.id2
+;
