f4f4ea4c65f462850eb6e850adc8427a779d1c4a
ash-shaik <fastmlai777@gmail.com>
2021-12-18 15:58:09 -0800

    sequences

--- numeric-sequences.sql
+++ numeric-sequences.sql
@@ -5,6 +5,7 @@
 
  */
 
+-- in MySQL cte_max_recursion_depth controls the upper limit of this bound in the query.
 WITH RECURSIVE seq AS
                        (SELECT 1 AS value UNION ALL SELECT value + 1 FROM seq WHERE value < 20)
 SELECT REPEAT('* ', value)
