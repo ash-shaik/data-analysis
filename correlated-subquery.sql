ccec13c8c9f3f80225b2925fd22021bbb3a33f2f
ash-shaik <fastmlai777@gmail.com>
2021-12-20 16:11:07 -0800

    correlated subquery

---
+++ correlated-subquery.sql
@@ -0,0 +1,9 @@
+/**
+  When we want order based metric, say per customer , their order amounts
+  - first
+  - first week
+  - first 2 weeks
+  Each customer's date is going to be different, so this is when a correlated subquery come in handy.
+  These can be run on the vtrbusic.payment and other public dataset on Mode Analytics.
+ */
+ 
