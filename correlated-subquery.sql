4e839961b929d8f5639c0d7c600ab0621b2138e3
ash-shaik <fastmlai777@gmail.com>
2021-12-20 16:36:01 -0800

    correlated subquery

--- correlated-subquery.sql
+++ correlated-subquery.sql
@@ -12,7 +12,7 @@ WITH customer_tx AS
          (SELECT customer_id, first_payment_date
           FROM (
                    SELECT p.customer_id
-                        , payment_date as first_payment_date
+                        , payment_date as                                                      first_payment_date
                         , row_number() OVER (PARTITION BY p.customer_id ORDER BY payment_date) rn
                    FROM vtrbusic.payment p) t
           WHERE rn = 1)
@@ -34,6 +34,47 @@ SELECT customer_tx.*
 
      , (SELECT SUM(p2.amount)
         FROM vtrbusic.payment p2
-        WHERE p2.customer_id = customer_tx.customer_id)  AS LTV                                             AS LTV
+        WHERE p2.customer_id = customer_tx.customer_id)          AS LTV
+    AS LTV
 
 FROM customer_tx
+
+
+/**
+  Taking it one step further to do a percentage of overall analysis
+ */
+
+WITH customer_tx AS
+         (SELECT customer_id, first_payment_date
+          FROM (
+                   SELECT p.customer_id
+                        , payment_date as first_payment_date
+                        , row_number() OVER (PARTITION BY p.customer_id ORDER BY payment_date) rn
+                   FROM vtrbusic.payment p) t
+          WHERE rn = 1)
+
+SELECT customer_id
+     , first_payment_date
+     , first_weeks_sales
+     , first_2week_sales
+     , ltv
+     , CONCAT(ROUND((first_weeks_sales / ltv) * 100), '%') AS first7PctOverall
+FROM (
+         SELECT customer_tx.*
+              , (SELECT SUM(p2.amount)
+                 FROM vtrbusic.payment p2
+                 WHERE p2.customer_id = customer_tx.customer_id
+                   AND p2.payment_date BETWEEN
+                     customer_tx.first_payment_date AND customer_tx.first_payment_date + INTERVAL '7 days')  AS first_weeks_sales
+
+              , (SELECT SUM(p2.amount)
+                 FROM vtrbusic.payment p2
+                 WHERE p2.customer_id = customer_tx.customer_id
+                   AND p2.payment_date BETWEEN
+                     customer_tx.first_payment_date AND customer_tx.first_payment_date + INTERVAL '14 days') AS first_2week_sales
+
+              , (SELECT SUM(p2.amount)
+                 FROM vtrbusic.payment p2
+                 WHERE p2.customer_id = customer_tx.customer_id)                                             AS LTV
+         FROM customer_tx) ct
+ORDER BY first_payment_date
