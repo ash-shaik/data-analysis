789be2c665c47b703b0b919401d2456f1c305897
ash-shaik <fastmlai777@gmail.com>
2021-12-30 21:38:35 -0800

    growth

--- time_windows.sql
+++ time_windows.sql
@@ -52,3 +52,22 @@ SELECT year_month
 FROM monthy_sales
 ;
 
+-- Percentage Growth of Sales
+WITH monthy_sales AS (SELECT extract('year' FROM p.payment_date) || '-'
+    || extract('month' FROM p.payment_date) AS year_month
+                           , SUM(amount)    as amount
+                      FROM vtrbusic.payment p
+                      GROUP BY extract('year' FROM p.payment_date) || '-'
+                                   || extract('month' FROM p.payment_date))
+
+   , mom_sales AS (SELECT year_month
+                        , amount
+                        , lag(year_month) OVER ( ORDER BY year_month) as prev_month
+                        , lag(amount) OVER ( ORDER BY year_month)     as prev_amount
+                   FROM monthy_sales)
+
+SELECT year_month
+     , amount
+     , (amount / prev_amount - 1) * 100 AS monthly_pct_growth
+FROM mom_sales
+;
